0e52d89fe74966e7bc660aaa5a0dc3a00e80aea9
icrona <aldomakmur@gmail.com>
2016-10-18 00:10:14 +0700

    tttttttttttttt

--- config/services.php
+++ config/services.php
@@ -16,7 +16,7 @@ return [
 
     'mailgun' => [
         'domain' => 'mg.skripsweet.xyz',
-        'secret' => 'dbbc8c5ff495742e7785708633d9501b',
+        'secret' => 'dbbc8c5ff495742e7785708633d9501b ',
     ],
 
     'ses' => [
