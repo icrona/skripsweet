76e2665cf713f1cce26714c3165f8084e27d9219
icrona <aldomakmur@gmail.com>
2016-10-21 20:19:54 +0700

    wwwform

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/getOrder',function(Request $request){
+	$image=$request->file('image');
+	$imageFileName = time() . '.' . $image->getClientOriginalExtension();
+
+	$s3=\Storage::disk('s3');
+	$filepath='/support-tickets'.$imageFileName;
+    $s3->put($filepath,file_get_contents($image),'public');
+    return view('welcome');
+});
