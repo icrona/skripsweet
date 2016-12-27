5ebb058f48099ed486f02358090132f337a638c7
icrona <aldomakmur@gmail.com>
2016-12-27 11:51:50 +0700

    update api profile

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -5,7 +5,7 @@
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
                 </button>
-                <a href={{url('/home')}}><img src="{{asset('images/logo/logo.png')}}" class="navbar-brand page-scroll" ></a>
+                <a href={{url('/home')}}><img src="{{asset('images/logo/'.$logo_name)}}" class="navbar-brand page-scroll" ></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
