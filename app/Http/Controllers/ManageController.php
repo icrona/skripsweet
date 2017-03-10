d17184206d71068f3a774fb780e96b71e01c9188
icrona <aldomakmur@gmail.com>
2017-03-10 17:31:05 +0700

    truncate version

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -27,6 +27,7 @@ class ManageController extends Controller
 {
 	public function showFlavour()
 	{
+        DB::table('versions')->truncate();
 		$flavours = Flavour::paginate(10);
 		$response = [
           'pagination' => [
